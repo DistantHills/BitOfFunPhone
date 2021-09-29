bca5f15aa29942ba7bfe1317396938521953b90f
EWarwick <83866607+DistantHills@users.noreply.github.com>
2021-09-29 15:52:45 +0100

    Add files via upload
    Layout now fixed (needed to set weights), and audio is working => good enough for a start

--- BitOfFunPhone.java
+++ BitOfFunPhone.java
@@ -4,9 +4,8 @@
 // Simple GUI app to mimic a phone - dialpad that plays 
 // sounds when you press the keys
 // All sound from https://freesound.org/ 
+// with Creative Commons licenses that allow their reuse
 // V1 Sep 2021 - Mastered on GitHub
-
-//package FunJava;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,14 +14,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.*;
 
-// @@@ Clean these out once working
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -36,6 +32,14 @@ public class BitOfFunPhone{
         private Integer mButtonDigit;
         private Clip mAudioClip;
         
+        ///////////////////////////////////////////////////
+        // DialPadButton()
+        // Constructor for a single button on the DialPad
+        // Inputs:
+        // - newButtonDigit - number on this button.  Also used to 
+        //                    find the associated audio clip
+        // Returns: nothing
+        ///////////////////////////////////////////////////
         public DialPadButton(Integer newButtonDigit) {
             super(Integer.toString(newButtonDigit));
             mButtonDigit = newButtonDigit;
@@ -52,7 +56,7 @@ public class BitOfFunPhone{
         // setupAudio()
         // Internal method to access the audio file and 
         // get ready to play it
-        // Returns - nothing
+        // Returns: nothing
         ///////////////////////////////////////////////////
         private void setupAudio() {
             String[] fileNames;
@@ -84,9 +88,8 @@ public class BitOfFunPhone{
                 DataLine.Info info = new DataLine.Info(Clip.class, format);     
                 mAudioClip = (Clip) AudioSystem.getLine(info);     
                 mAudioClip.open(audioStream);
-                 
+                
                 System.out.println("Button " + mButtonDigit + " ready to play - " + wavFileName);
-                 
             } catch (UnsupportedAudioFileException ex) {
                 System.out.println("The specified audio file is not supported.");
                 ex.printStackTrace();
@@ -103,6 +106,9 @@ public class BitOfFunPhone{
         // actionPerformed()
         // For the EventListener interface, so we know 
         // when this button is pressed
+        // Inputs:
+        // - e - information about the event
+        // Returns: nothing
         ///////////////////////////////////////////////////
         public void actionPerformed(ActionEvent e) {
             // Make sure we always play from the start
@@ -114,25 +120,6 @@ public class BitOfFunPhone{
             mAudioClip.start();
         }
         
-        // @@@ may still want this - not clear if we need to clip.close() everytime we 
-        // stop playing, or just at end of day
-        // /**
-         // * Listens to the START and STOP events of the audio line.
-         // // @@@ May move to inner class as part of button press
-         // */
-        // @Override
-        // public void update(LineEvent event) {
-            // LineEvent.Type type = event.getType();
-             
-            // if (type == LineEvent.Type.START) {
-                // System.out.println("Playback started.");
-                 
-            // } else if (type == LineEvent.Type.STOP) {
-                // playCompleted = true;
-                // System.out.println("Playback completed.");
-            // } 
-        // }
-
         ///////////////////////////////////////////////////
         // finalize() - has been deprecated.  So, we can't explicitly 
         // close the audio Clip, but will assume garbage collection will do so
@@ -145,6 +132,7 @@ public class BitOfFunPhone{
     // Sets up the DialPad panel with buttons
     // Could potentially expand in future, e.g. with a 'screen'
     // and dial button, so put the dialpad on its own panel
+    // Returns: the JPanel with all the BitOfFunPhone content
     ///////////////////////////////////////////////////
     public JPanel createContentPane(){
 
@@ -157,21 +145,23 @@ public class BitOfFunPhone{
         // dialpad on my phone, so 1->9 
         // in a grid and 0 at the bottom middle
         JPanel dialPadPanel = new JPanel();
-        // dialPadPanel.setLayout(new GridBagLayout());
-        // GridBagConstraints cons = new GridBagConstraints();
-        
-        dialPadPanel.setLayout(new GridLayout(4,3,10,10));
-        dialPadPanel.setSize(500,600);
+        dialPadPanel.setBackground(Color.blue);
+        dialPadPanel.setLayout(new GridBagLayout());
+        // All buttons behave the same wrt resizing.
+        // We just use the constraints to control where each button goes
+        GridBagConstraints cons = new GridBagConstraints();
+        cons.weightx = 0.5;
+        cons.weighty = 0.5;
+        cons.fill = GridBagConstraints.BOTH;
         
         for (int ii = 1; ii <= 9; ii++){
-            // cons.gridx = ii-1;
-            // cons.gridy = 0;
-            // cons.fill = GridBagConstraints.BOTH;
-            // dialPadPanel.add(new DialPadButton(ii), cons);
-            dialPadPanel.add(new DialPadButton(ii));
+            cons.gridx = (ii-1)%3;
+            cons.gridy = (int)Math.floor((ii-1)/3);
+            dialPadPanel.add(new DialPadButton(ii), cons);
         }
-        
-        dialPadPanel.add(new DialPadButton(0));
+        cons.gridx = 1;
+        cons.gridy = 3;
+        dialPadPanel.add(new DialPadButton(0), cons);
         
         totalPhonePanel.add(dialPadPanel);
         totalPhonePanel.setOpaque(true);
@@ -179,6 +169,10 @@ public class BitOfFunPhone{
     }
 
 
+    ///////////////////////////////////////////////////
+    // createAndShowGUI()
+    // Set up the overall application window
+    ///////////////////////////////////////////////////
     private static void createAndShowGUI() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Bit Of Fun Phone");
@@ -193,6 +187,10 @@ public class BitOfFunPhone{
         frame.setVisible(true);
     }
 
+    ///////////////////////////////////////////////////
+    // main ()
+    // Entry point
+    ///////////////////////////////////////////////////
     public static void main(String[] args) {        
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
