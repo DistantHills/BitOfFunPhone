dbe533a919328675702192fe60ab842bc04d5e5a
EWarwick <83866607+DistantHills@users.noreply.github.com>
2021-09-29 14:51:15 +0100

    Add files via upload
    Odd audio behaviour sorted (removed code that halted processing until audio complete).  Now responds promptly and definitely can play multiple audio in parallel  Should probably add handling to make sure we either stop & restart audio if the button is pressed again, or just ignore the press if we're busy doing audio.  I won't worry about multiple buttons playing at same time though.  Next work - sort out the layout

--- BitOfFunPhone.java
+++ BitOfFunPhone.java
@@ -28,20 +28,13 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class BitOfFunPhone{
     ///////////////////////////////////////////////////
-    // Global class variables
-
+    // DialPadButton() inner class
+    // We'll create 1 for each dial pad button, so
+    // we can keep the per-button processing together
     ///////////////////////////////////////////////////
-    // DialPadButton class
-    // We will have 10 very similar dial pad buttons
-    // so create a class, so we can listen to
-    // each button separately
-    // """ Note to self - could potentially extend JButton, rather than
-    // having it as a property
-    ///////////////////////////////////////////////////
-    class DialPadButton extends JButton implements  ActionListener, LineListener{
+    class DialPadButton extends JButton implements  ActionListener{
         private Integer mButtonDigit;
         private Clip mAudioClip;
-        boolean playCompleted;
         
         public DialPadButton(Integer newButtonDigit) {
             super(Integer.toString(newButtonDigit));
@@ -55,27 +48,12 @@ public class BitOfFunPhone{
             addActionListener(this);            
         }
 
-        // Called when something happens to this button
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("Something happened");
-            // Make sure we always play from the start
-            mAudioClip.setFramePosition(0);
-            mAudioClip.start();
-             
-            while (!playCompleted) {
-                // wait for the playback completes
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-             
-           // Don't close until finished @@ for now.  How to close on shutdown??
-//           mAudioClip.close();
-        }
-        
-        // Method to load the audio clip for this button
+        ///////////////////////////////////////////////////
+        // setupAudio()
+        // Internal method to access the audio file and 
+        // get ready to play it
+        // Returns - nothing
+        ///////////////////////////////////////////////////
         private void setupAudio() {
             String[] fileNames;
             String wavFileName;
@@ -83,7 +61,6 @@ public class BitOfFunPhone{
             String soundDirName = "Sounds-numbers";
                 
             // Find a *.wav file with a <thisDigit>_ prefix
-            System.out.println("@@ setupAudio");
             File soundDirectory = new File("Sounds-numbers");        
             String namePrefix = Integer.toString(mButtonDigit) + "_";
             FilenameFilter filter = new FilenameFilter() {
@@ -97,23 +74,18 @@ public class BitOfFunPhone{
             // Ideally put more error handling in here, since we're dealing with external files
             fileNames = soundDirectory.list(filter);
             wavFileName = fileNames[0];
-            System.out.println(wavFileName);
             
             // Windows-specific.  This should be updated so it's OS-independent
             File audioFile = new File(soundDirName + "/" + wavFileName);
      
-            // @@@ testing.  For moment play on creation.  Later play on button press
             try {
                 AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
                 AudioFormat format = audioStream.getFormat();
                 DataLine.Info info = new DataLine.Info(Clip.class, format);     
-                mAudioClip = (Clip) AudioSystem.getLine(info);
-     
-     // @@@ currently don't care when audio finishes
-                mAudioClip.addLineListener(this);
+                mAudioClip = (Clip) AudioSystem.getLine(info);     
                 mAudioClip.open(audioStream);
                  
-                System.out.println("Ready to start clip");
+                System.out.println("Button " + mButtonDigit + " ready to play - " + wavFileName);
                  
             } catch (UnsupportedAudioFileException ex) {
                 System.out.println("The specified audio file is not supported.");
@@ -127,39 +99,75 @@ public class BitOfFunPhone{
             }
         }
 
-        /**
-         * Listens to the START and STOP events of the audio line.
-         // @@@ May move to inner class as part of button press
-         */
-        @Override
-        public void update(LineEvent event) {
-            LineEvent.Type type = event.getType();
-             
-            if (type == LineEvent.Type.START) {
-                System.out.println("Playback started.");
-                 
-            } else if (type == LineEvent.Type.STOP) {
-                playCompleted = true;
-                System.out.println("Playback completed.");
-            } 
+        ///////////////////////////////////////////////////
+        // actionPerformed()
+        // For the EventListener interface, so we know 
+        // when this button is pressed
+        ///////////////////////////////////////////////////
+        public void actionPerformed(ActionEvent e) {
+            // Make sure we always play from the start
+            mAudioClip.setFramePosition(0);
+            
+            // Note that this will create a new thread to 
+            // play the audio, so it won't block processing on the
+            // main thread, and multiple audio clips can play in parallel
+            mAudioClip.start();
         }
+        
+        // @@@ may still want this - not clear if we need to clip.close() everytime we 
+        // stop playing, or just at end of day
+        // /**
+         // * Listens to the START and STOP events of the audio line.
+         // // @@@ May move to inner class as part of button press
+         // */
+        // @Override
+        // public void update(LineEvent event) {
+            // LineEvent.Type type = event.getType();
+             
+            // if (type == LineEvent.Type.START) {
+                // System.out.println("Playback started.");
+                 
+            // } else if (type == LineEvent.Type.STOP) {
+                // playCompleted = true;
+                // System.out.println("Playback completed.");
+            // } 
+        // }
+
+        ///////////////////////////////////////////////////
+        // finalize() - has been deprecated.  So, we can't explicitly 
+        // close the audio Clip, but will assume garbage collection will do so
+        ///////////////////////////////////////////////////
     }
 
 
-    public JPanel createContentPane (){
+    ///////////////////////////////////////////////////
+    // createContentPane()
+    // Sets up the DialPad panel with buttons
+    // Could potentially expand in future, e.g. with a 'screen'
+    // and dial button, so put the dialpad on its own panel
+    ///////////////////////////////////////////////////
+    public JPanel createContentPane(){
 
         // We create a bottom JPanel to place everything on.
         JPanel totalPhonePanel = new JPanel();
-        totalPhonePanel.setLayout(null);
+        // Use GridLayout so the content expands to fill the space
+        totalPhonePanel.setLayout(new GridLayout(1,1,10,10));
 
         // Set up the dialpad with a similar layout to the 
         // dialpad on my phone, so 1->9 
         // in a grid and 0 at the bottom middle
         JPanel dialPadPanel = new JPanel();
+        // dialPadPanel.setLayout(new GridBagLayout());
+        // GridBagConstraints cons = new GridBagConstraints();
+        
         dialPadPanel.setLayout(new GridLayout(4,3,10,10));
-        dialPadPanel.setSize(500,500);
+        dialPadPanel.setSize(500,600);
         
         for (int ii = 1; ii <= 9; ii++){
+            // cons.gridx = ii-1;
+            // cons.gridy = 0;
+            // cons.fill = GridBagConstraints.BOTH;
+            // dialPadPanel.add(new DialPadButton(ii), cons);
             dialPadPanel.add(new DialPadButton(ii));
         }
         
@@ -180,7 +188,8 @@ public class BitOfFunPhone{
         frame.setContentPane(myPhone.createContentPane());
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(250, 300);
+        frame.setSize(500, 600);
+        frame.pack();
         frame.setVisible(true);
     }
 
